0926ba834acb105081a03d16423fdba59accf6ec
Milye-verdin06 <milagros.espinosa.verdin@gmail.com>
2021-04-06 17:33:08 -0500

    falla barra de navegacio y no implementa angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +28,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RouterModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatIconModule
+    
   ],
+  
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
--- src/app/pages/agregar-articulos/agregar-articulos.component.ts
+++ src/app/pages/agregar-articulos/agregar-articulos.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-agregar-articulos',
   templateUrl: './agregar-articulos.component.html',
   
 })
 export class AgregarArticulosComponent implements OnInit {
+ 
 
-  constructor() { }
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor(
+
+  ) {
+    
+
+   }
 
   ngOnInit(): void {
   }
